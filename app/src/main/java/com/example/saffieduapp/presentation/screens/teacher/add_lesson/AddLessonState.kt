a8d2632d39f5d89b86a15188c2bafbe850fa73eb
taherzeiad <tahrqdyh@gmail.com>
2025-09-27 14:45:10 +0300

    some editing

--- app/src/main/java/com/example/saffieduapp/presentation/screens/teacher/add_lesson/AddLessonState.kt
+++ app/src/main/java/com/example/saffieduapp/presentation/screens/teacher/add_lesson/AddLessonState.kt
@@ -10,12 +10,15 @@ data class AddLessonState(
     val lessonTitle: String = "",
     val description: String = "",
     val selectedClass: String = "",
-    val selectedVideoUri: Uri? = null,
-    val selectedPdfUri: Uri? = null,
+    val selectedVideoUriString: String? = null, // حفظ كـ String
+    val selectedPdfUriString: String? = null,   // حفظ كـ String
     val selectedVideoName: String? = null,
     val selectedPdfName: String? = null,
     val publicationDate: String = "",
     val notifyStudents: Boolean = false,
     val isSaving: Boolean = false,
     val selectedContentType: ContentType = ContentType.NONE
-)
+) {
+    val selectedVideoUri: Uri? get() = selectedVideoUriString?.let { Uri.parse(it) }
+    val selectedPdfUri: Uri? get() = selectedPdfUriString?.let { Uri.parse(it) }
+}
