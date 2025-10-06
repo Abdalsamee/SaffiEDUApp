5b5828eeee6ec956fe8d19e2981e44f404b197ae
taherzeiad <tahrqdyh@gmail.com>
2025-10-06 13:12:13 +0300

    display question in summary screen

--- app/src/main/java/com/example/saffieduapp/navigation/CreateQuizNavGraph.kt
+++ app/src/main/java/com/example/saffieduapp/navigation/CreateQuizNavGraph.kt
@@ -1,13 +1,18 @@
 package com.example.saffieduapp.navigation
 
+import android.annotation.SuppressLint
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.example.saffieduapp.presentation.screens.teacher.add_exam.AddExamScreen
 import com.example.saffieduapp.presentation.screens.teacher.add_question.AddQuestionScreen
+import com.example.saffieduapp.presentation.screens.teacher.add_question.AddQuestionViewModel
+import com.example.saffieduapp.presentation.screens.teacher.add_question.QuestionData
 import com.example.saffieduapp.presentation.screens.teacher.quiz_summary.QuizSummaryScreen
 
+@SuppressLint("StateFlowValueCalledInComposition")
 fun NavGraphBuilder.createQuizNavGraph(navController: NavController) {
     navigation(
         startDestination = Routes.ADD_EXAM_SCREEN,
@@ -27,22 +32,27 @@ fun NavGraphBuilder.createQuizNavGraph(navController: NavController) {
         // الوجهة الثانية: إضافة الأسئلة
         composable(Routes.ADD_QUESTION_SCREEN) {
             AddQuestionScreen(
+                navController = navController, // ✅ أضف هذا
                 onNavigateUp = { navController.popBackStack() },
                 onNavigateToSummary = {
                     navController.navigate(Routes.QUIZ_SUMMARY_SCREEN)
                 }
             )
-        }
+    }
 
         // الوجهة الثالثة: ملخص الأسئلة
-        composable(Routes.QUIZ_SUMMARY_SCREEN) {
+        composable(Routes.QUIZ_SUMMARY_SCREEN) { backStackEntry ->
+            val questions = navController.previousBackStackEntry
+                ?.savedStateHandle
+                ?.get<List<QuestionData>>("questions") ?: emptyList()
+
             QuizSummaryScreen(
+                questions = questions,
                 onNavigateUp = { navController.popBackStack() },
                 onPublish = {
-
                     navController.popBackStack(Routes.CREATE_QUIZ_GRAPH, inclusive = true)
                 }
             )
-        }
+    }
     }
 }
