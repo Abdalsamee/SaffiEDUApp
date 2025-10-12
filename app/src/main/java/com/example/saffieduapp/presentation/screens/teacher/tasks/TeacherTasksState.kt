879f001bb2b04e20e087d34de0e68eee0e556a33
Abdalsamee <abd409115011@gmail.com>
2025-10-12 12:22:37 +0300

    edit teacher task 0.1

--- app/src/main/java/com/example/saffieduapp/presentation/screens/teacher/tasks/TeacherTasksState.kt
+++ app/src/main/java/com/example/saffieduapp/presentation/screens/teacher/tasks/TeacherTasksState.kt
@@ -1,11 +1,20 @@
 package com.example.saffieduapp.presentation.screens.teacher.tasks
 
-
-enum class TeacherTasksTab {
-    HOMEWORKS,
-    EXAMS
-}
+import com.example.saffieduapp.presentation.screens.teacher.tasks.components.TaskType
 
 data class TeacherTasksState(
-    val selectedTabIndex: Int = 0 // 0 = الواجبات, 1 = الاختبارات
+    val selectedTabIndex: Int = 0,
+    val selectedClass: String = "الصف السادس",
+    val isLoading: Boolean = false,
+    val assignments: List<TeacherTaskItem> = emptyList(),
+    val exams: List<TeacherTaskItem> = emptyList()
+)
+
+data class TeacherTaskItem(
+    val id: String = "",
+    val subject: String,
+    val date: String,
+    val time: String,
+    val isActive: Boolean,
+    val type: TaskType
 )
